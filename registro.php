9e7711a42b61dbccb885b17e8fb4e518a9782136
uriieelsosa <sosauriel1996@gmail.com>
2025-09-09 02:12:11 -0300

    login y registro
    mejoras en el diseño de la pag de login. agreago de la pag de registro y su php basico

---
+++ registro.php
@@ -0,0 +1,16 @@
+<?php
+
+    $nombre = $_POST["nombre"];
+    $mail = $_POST["mail"];
+    $contra = $_POST["contra"];
+    $contra2 = $_POST["contra2"];     
+
+    // Aquí puedes agregar la lógica para procesar el registro, como guardar los datos en una base de datos
+    $conexion = mysqli_connect("localhost", "root", "", "mayorista-platanos");
+    $query = "INSERT INTO usuario (nombre, mail, contra, activo, admin) VALUES ('$nombre', '$mail', '$contra', 1, 0)";
+    mysqli_query($conexion, $query);
+    mysqli_close($conexion);
+    header("Location: login.php");
+    exit;
+
+?>
