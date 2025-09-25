14c8833617bbe8c4216b0c052b176dfda0c4c9d6
uriieelsosa <sosauriel1996@gmail.com>
2025-09-24 21:24:27 -0300

    registro usuarios admi

--- pag-web/registro.php
+++ pag-web/registro.php
@@ -6,7 +6,7 @@
     $contra2 = $_POST["contra2"];     
 
     // Aquí puedes agregar la lógica para procesar el registro, como guardar los datos en una base de datos
-    $conexion = mysqli_connect("localhost", "root", "", "mayorista-platanos");
+    $conexion = mysqli_connect('localhost', 'root', '', 'mayorista-platanos');
     $query = "INSERT INTO usuario (nombre, mail, contra, activo, admin) VALUES ('$nombre', '$mail', '$contra', 1, 0)";
     mysqli_query($conexion, $query);
     mysqli_close($conexion);
